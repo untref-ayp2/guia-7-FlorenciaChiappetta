d43abb3eb5beedb96a8b15f1ef144de107f24f03
FlorenciaChiappetta <111155028+FlorenciaChiappetta@users.noreply.github.com>
2023-07-11 01:00:53 -0300

    Update ej11 IMPLEMENTAR.go

--- ejercicios/ej11 IMPLEMENTAR.go
+++ ejercicios/ej11 IMPLEMENTAR.go
@@ -4,7 +4,14 @@ package ejercicios
 // n y b y devuelva true si n es potencia de b.
 // Por ejemplo: esPotencia(8, 2) devuelve true.
 func EsPotencia(n, b int) bool {
-   
+   if n == 1 {
+        return true
+    } else if n < b {
+        return false
+    } else {
+        return esPotencia(n/b, b)
+    }
+}
 
 func potenciaRecursiva(base float64, exponente int) float64 {
     if exponente == 0 {
